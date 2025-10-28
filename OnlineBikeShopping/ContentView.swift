5bee5f4dca72d378d503b0763b7aea50eeaa293e
duclh11 <huyducle1109@gmail.com>
2025-10-28 22:41:11 +0700

    test gitaction

--- OnlineBikeShopping/ContentView.swift
+++ OnlineBikeShopping/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     var body: some View {
         CustomTabBarView(tabItems: [
             TabItem(view: AnyView(MainScreen()), icon: AnyView(Image("item_nav1"))),
-            TabItem(view: AnyView(SampleView(color: .red, content: "red screen")), icon: AnyView(Image("item_nav2"))),
+            TabItem(view: AnyView(SampleView(color: .red, content: "red1 screen")), icon: AnyView(Image("item_nav2"))),
             TabItem(view: AnyView(SampleView(color: .green, content: "green screen")), icon: AnyView(Image("item_nav3"))),
             TabItem(view: AnyView(SampleView(color: .blue, content: "blue screen")), icon: AnyView(Image("item_nav4"))),
         ])
